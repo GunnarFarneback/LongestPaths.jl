56c336d30e992c2388696c562b1cbf208a0dc5f8
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2021-10-03 11:11:06 +0200

    Update for Cbc 0.7.

--- src/longest_path.jl
+++ src/longest_path.jl
@@ -1136,49 +1136,57 @@ end
 
 function solve_IP(O::OptProblem, initial_solution = Int[],
                   use_warmstart = true; options...)
-    model = Cbc.CbcCInterface.CbcModel()
-    Cbc.CbcCInterface.setParameter(model, "logLevel", "0")
+    model = Cbc.Cbc_newModel()
+    Cbc.Cbc_setParameter(model, "logLevel", "0")
     for (name, value) in options
-        Cbc.CbcCInterface.setParameter(model, string(name), string(value))
+        Cbc.Cbc_setParameter(model, string(name), string(value))
     end
 
     A, lb, ub = add_cycle_constraints_to_formulation(O)
-    Cbc.CbcCInterface.loadProblem(model, A, O.l, O.u, O.c, lb, ub)
-    Cbc.CbcCInterface.setObjSense(model, -1) # Maximize
+    cbc_loadproblem!(model, A, O.l, O.u, O.c, lb, ub)
+    Cbc.Cbc_setObjSense(model, -1) # Maximize
 
     for i in 1:size(A, 2)
-        Cbc.CbcCInterface.setInteger(model, i - 1)
+        Cbc.Cbc_setInteger(model, i - 1)
     end
     if !isempty(initial_solution) && use_warmstart
-        Cbc.CbcCInterface.setMIPStartI(model, collect(Cint.(eachindex(initial_solution) .- 1)),
-                                       initial_solution)
+        Cbc.Cbc_setMIPStartI(model, length(initial_solution),
+                             collect(Cint.(eachindex(initial_solution) .- 1)),
+                             Float64.(initial_solution))
     end
-    Cbc.CbcCInterface.solve(model)
+    Cbc.Cbc_solve(model)
    
     attrs = Dict()
-    attrs[:objbound] = Cbc.CbcCInterface.getBestPossibleObjValue(model)
+    attrs[:objbound] = Cbc.Cbc_getBestPossibleObjValue(model)
     attrs[:solver] = :ip
-    solution = Solution(cbc_status(model), Cbc.CbcCInterface.getObjValue(model),
-                        Cbc.CbcCInterface.getColSolution(model), attrs)
+    solution = Solution(cbc_status(model), Cbc.Cbc_getObjValue(model),
+                        copy(unsafe_wrap(Array,
+                                         Cbc.Cbc_getColSolution(model),
+                                         (size(A, 2),))), attrs)
+    Cbc.Cbc_deleteModel(model)
     return solution
 end
 
+function cbc_loadproblem!(model, A, l, u, c, lb, ub)
+    Cbc.Cbc_loadProblem(model, size(A, 2), size(A, 1),
+                        Cbc.CoinBigIndex.(A.colptr .- 1),
+                        Int32.(A.rowval .- 1),
+                        Float64.(A.nzval),
+                        Float64.(l), Float64.(u), Float64.(c),
+                        Float64.(lb), Float64.(ub))
+end
+
 function cbc_status(model)
-    if Cbc.CbcCInterface.isProvenOptimal(model)
-        return :Optimal
-    elseif Cbc.CbcCInterface.isProvenInfeasible(model)
-        return :Infeasible
-    elseif Cbc.CbcCInterface.isContinuousUnbounded(model)
-        return :Unbounded # is this correct?
-    elseif (Cbc.CbcCInterface.isNodeLimitReached(model) ||
-            Cbc.CbcCInterface.isSecondsLimitReached(model) ||
-            Cbc.CbcCInterface.isSolutionLimitReached(model))
-        return :UserLimit
-    elseif Cbc.CbcCInterface.isAbandoned(model)
-        return :Error
-    else
-        return :InternalError
+    for (predicate, value) in ((Cbc.Cbc_isProvenOptimal, :Optimal),
+                               (Cbc.Cbc_isProvenInfeasible, :Infeasible),
+                               (Cbc.Cbc_isContinuousUnbounded, :Unbounded),
+                               (Cbc.Cbc_isNodeLimitReached, :UserLimit),
+                               (Cbc.Cbc_isSecondsLimitReached, :UserLimit),
+                               (Cbc.Cbc_isSolutionLimitReached, :UserLimit),
+                               (Cbc.Cbc_isAbandoned, :Error))
+        predicate(model) != 0 && return value
     end
+    return :InternalError
 end
 
 function add_cycle_constraints_to_formulation(O::OptProblem)
